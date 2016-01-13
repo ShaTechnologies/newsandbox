3d227208733fdfe3ab30879bd518c423e4b1a9ce
kenthellck <praveengunaratna@yahoo.com>
2016-01-13 13:16:15 +0530

    sample class :D

--- ck.php
+++ ck.php
@@ -5,6 +5,25 @@
  * Date: 1/13/2016
  * Time: 10:17 AM
  */
-echo "hi im praveen";
-echo "hi shavindra";
+
+ class vehicle{
+    var $name;
+    var $speed;
+
+    function setname($name,$speed)
+    {
+        $this->name=$name;
+        $this->speed=$speed;
+    }
+function printthis()
+{
+    echo 'speed is '. $this->speed.' vehicle is '. $this->name;
+
+}
+
+}
+$v1 =new vehicle();
+$v1->setname('hyundai','180');
+$v1->printthis();
+
 ?>
